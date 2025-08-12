d2f7031a92a4c59104ef6a79827455987720fcfc
Daniel Lin <146984744+notindenial@users.noreply.github.com>
2025-08-12 18:21:59 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Portfolio",
+  DESCRIPTION: "Welcome to my portfolio landing page.",
+  AUTHOR: "Daniel Lin",
 }
 
 // Work Page
@@ -56,26 +56,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "dlin3@andrew.cmu.edu",
+    HREF: "mailto:dlin3@andrew.cmu.edu",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "notindenial",
+    HREF: "https://github.com/notindenial"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
     TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    HREF: "https://www.linkedin.com/in/daniel-lin-70b8b2264/",
   },
 ]
 
